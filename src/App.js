4a0f5011d7f39e6887075ac47789f4e5ffa5d8cc
lucasrohlf <lucas@poundcreative.com>
2020-06-12 09:13:05 -0700

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Guck off
+          Edit <code>src/App.js</code> and save to reload. Feature.
         </p>
         <a
           className="App-link"
